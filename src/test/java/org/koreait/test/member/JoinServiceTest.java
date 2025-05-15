6759b7a11d1c41dcb04837e3687794cefb94b1ff
KimMarinet <kiana507680@gmail.com>
2025-05-16 02:34:10 +0900

    20250516_2

--- src/test/java/org/koreait/test/member/JoinServiceTest.java
+++ src/test/java/org/koreait/test/member/JoinServiceTest.java
@@ -1,5 +1,6 @@
 package org.koreait.test.member;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,37 +9,69 @@ import org.koreait.member.controllers.RequestJoin;
 import org.koreait.member.services.JoinService;
 import org.koreait.member.validators.JoinValidator;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.lang.reflect.Method;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("회원 가입 기능 테스트")
 public class JoinServiceTest {
 
     private  JoinService service;
 
+    private RequestJoin getSuccessData(){
+        Faker faker = new Faker(Locale.KOREAN);
+        return RequestJoin.builder()
+                .email(faker.internet().emailAddress())
+                .password("123456789")
+                .build();
+    }
+
+    private RequestJoin getFailureData(String field, String value){
+        RequestJoin obj = getSuccessData();
+        for (Method method : RequestJoin.class.getDeclaredMethods()){
+            System.out.println(method.getName());
+            String name = method.getName();
+            if(name.equalsIgnoreCase("set" + field)){
+                try {
+                    method.invoke(obj, value);
+                }catch (Exception e){
+                    break;
+                }
+            }
+        }
+        return obj;
+    }
+
     @BeforeEach
     void init(){
         service = new JoinService(new JoinValidator());
+        System.out.println(getSuccessData());
     }
 
     @Test
     @DisplayName("회원 가입 성공 시 예외가 발생하지 않는다")
     void successTest(){
         //JoinService service = new JoinService(new JoinValidator());
-        RequestJoin form = new RequestJoin();
-        form.setEmail("user01@test.org");
-        form.setPassword("123456789");
+        //RequestJoin form = new RequestJoin();
+//        form.setEmail("user01@test.org");
+//        form.setPassword("123456789");
         assertDoesNotThrow(()->{
-            service.process(form);
+            service.process(getSuccessData());
         });
     }
 
     @Test
     @DisplayName("필수 항목 입력 X => 예외 처리 발생")
     void requiredFieldValidationTest(){
-        RequestJoin form = new RequestJoin();
-        assertThrows(BadRequestException.class, ()->{
-           service.process(form);
-        });
+        assertAll(
+                () -> assertThrows(BadRequestException.class, ()->{
+                service.process(getFailureData("email", null));
+                }),
+
+                () -> assertThrows(BadRequestException.class, ()->{
+                service.process(getFailureData("password", null));
+                })
+        );
     }
 }
